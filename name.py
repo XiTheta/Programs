9a33a491dd61a81014f9975dc227cea4ff22157c
XiTheta <98294666+XiTheta@users.noreply.github.com>
2022-01-23 20:16:53 -0600

    Add files via upload

---
+++ name.py
@@ -0,0 +1,7 @@
+print('Enter your name: ',end='')
+name_name = input()
+if name_name == 'Phos':
+    print('Suffer')
+else:
+    print('Have a nice day,',name_name,'!')
+input('Press ENTER to exit')
